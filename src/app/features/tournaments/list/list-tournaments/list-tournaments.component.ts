eb91ead661b373189fe31f2cdc8bf494f8b5e13d
Alic <alic.victor@academico.ifpb.edu.br>
2025-01-29 11:36:06 -0300

    feat: inicio da listagem

--- src/app/features/tournaments/list/list-tournaments/list-tournaments.component.ts
+++ src/app/features/tournaments/list/list-tournaments/list-tournaments.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CardMainComponent } from '../../../../shared/components/card-main/card-main.component';
+import { TournamentCardComponent } from '../../../../shared/components/tournament-card/tournament-card.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-list-tournaments',
   imports: [
-    CardMainComponent
+    CardMainComponent,
+    TournamentCardComponent,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   templateUrl: './list-tournaments.component.html',
-  styleUrl: './list-tournaments.component.scss'
+  styleUrl: './list-tournaments.component.scss',
 })
 export class ListTournamentsComponent {
 
