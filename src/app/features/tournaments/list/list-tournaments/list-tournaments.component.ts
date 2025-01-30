29c65e7593642c8f57f2b1266eeec33129c3cb56
alic <alicvictor11@gmail.com>
2025-01-30 19:11:15 -0300

    fix: styling using flex

--- src/app/features/tournaments/list/list-tournaments/list-tournaments.component.ts
+++ src/app/features/tournaments/list/list-tournaments/list-tournaments.component.ts
@@ -19,4 +19,26 @@ import {MatInputModule} from '@angular/material/input';
 })
 export class ListTournamentsComponent {
 
+  tournaments = [
+    {
+      title: "Title 1",
+      subtitle: "subtitle 1",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio saepe a praesentium aperiam, esse quam vero eligendi pariatur molestias magnam accusamus quisquam enim. Dignissimos voluptates non ex corporis aliquam consequuntur!"
+    },
+    {
+      title: "Title 2",
+      subtitle: "subtitle 2",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio saepe a praesentium aperiam, esse quam vero eligendi pariatur molestias magnam accusamus quisquam enim. Dignissimos voluptates non ex corporis aliquam consequuntur!"
+    },
+    {
+      title: "Title 3",
+      subtitle: "subtitle 3",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio saepe a praesentium aperiam, esse quam vero eligendi pariatur molestias magnam accusamus quisquam enim. Dignissimos voluptates non ex corporis aliquam consequuntur!"
+    },
+    {
+      title: "Title 4",
+      subtitle: "subtitle 4",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio saepe a praesentium aperiam, esse quam vero eligendi pariatur molestias magnam accusamus quisquam enim. Dignissimos voluptates non ex corporis aliquam consequuntur!"
+    }
+  ]
 }
