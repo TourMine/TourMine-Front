032b2b647253eb33f98953406e97b824f2dd59e5
Gustavo Nascimento <santos025gn@gmail.com>
2025-01-31 01:26:06 -0300

    feat: list tournament

--- src/app/features/tournaments/list/list-tournaments/list-tournaments.component.ts
+++ src/app/features/tournaments/list/list-tournaments/list-tournaments.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TournamentServiceService } from '../../../../services/tournament/tournament-service.service';
+import { CommonModule } from '@angular/common';
 import { CardMainComponent } from '../../../../shared/components/card-main/card-main.component';
+
 
 @Component({
   selector: 'app-list-tournaments',
   imports: [
+    CommonModule,
     CardMainComponent
   ],
   templateUrl: './list-tournaments.component.html',
   styleUrl: './list-tournaments.component.scss'
 })
-export class ListTournamentsComponent {
+export class ListTournamentsComponent implements OnInit {
+  tournaments: any[] = [];
+  
+  constructor(private tournamentService: TournamentServiceService) {}
 
+  ngOnInit() {
+    this.listAllTournaments();
+  }
+
+
+  listAllTournaments() {
+    this.tournamentService.getAllTournaments().subscribe({
+      next: (data: any) => {
+        this.tournaments = data.items;
+        console.log('Torneios carregados:', this.tournaments);
+      },
+      error: (error) => {
+        console.error('Erro ao buscar torneios', error);
+      }
+    });
+  }
 }
