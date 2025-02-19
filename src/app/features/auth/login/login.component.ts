46382a6dcee1669348fb12c5d21cd3bd092b3f37
alic <alicvictor11@gmail.com>
2025-02-18 22:31:21 -0300

    feat: secao para login e signup

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { FormBuilder, FormsModule, Validators } from '@angular/forms';
@@ -6,6 +6,8 @@ import { ButtonModule } from 'primeng/button';
 import { Router, RouterModule } from '@angular/router';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ImageModule } from 'primeng/image';
+import { Carousel } from 'primeng/carousel';
 import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
@@ -17,10 +19,12 @@ import { AuthService } from '../../../services/auth/auth.service';
     FormsModule,
     ButtonModule,
     ReactiveFormsModule,
+    ImageModule,
+    Carousel,
     RouterModule
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   loginForm: FormGroup;
@@ -28,6 +32,20 @@ export class LoginComponent {
   password!: string;
 
   loading: boolean = false;
+
+  images: string[] = [
+    'assets/images/csgo.jpg',
+    'assets/images/fifa.jpg',
+    'assets/images/fortnite.jpg',
+    'assets/images/lol.jpg',
+    'assets/images/valorant.jpg'
+  ];
+
+  responsiveOptions: any[] = [
+    { breakpoint: '1024px', numVisible: 3, numScroll: 1 },
+    { breakpoint: '768px', numVisible: 2, numScroll: 1 },
+    { breakpoint: '576px', numVisible: 1, numScroll: 1 }
+  ];
 
   constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {
     this.loginForm = this.fb.group({
